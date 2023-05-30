c38ab022df665b38ca8d3e3c91cbb845c40220bc
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2023-05-31 04:15:56 +0500

    Added Comments
    Added Comments for understanding of anyone they can understand what's happening in the code.

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ st.markdown(
         ### This app let you take screenshot and automatically convert screenshot of any website to webp
     """
     )
+#Input field to take url
 url_ss = st.text_input("Add Url of site you want to take screenshot of.", key=None, type="default", placeholder="Url of the website eg: https://www.google.com", label_visibility="visible")
+#Input Field to take name for screenshot
 name_of_site = st.text_input("Add name of the screenshot *This is optional.", key=None, type="default", placeholder="Google", label_visibility="visible")
+
+#Button to take screenshot
 result = st.button("Take Screenshot")
 
+#Function to declare headless chrome driver used to access website
 def web_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--verbose")
@@ -24,25 +29,27 @@ def web_driver():
     driver = webdriver.Chrome(options=options)
     return driver
 
+#Call to function to declare headless chrome
 driver = web_driver()
 
+
+#Function to convert screenshot to webp
 def convert_to_webp(source, title):
-    if(title == ""):
+    if(title == ""): #Check if title is not empty
         title = "Screenshot"
-    #destination = source.with_suffix(".webp")
     destination = f"screenshots/{title}.webp"
 
     image = Image.open(source)  # Open image
     image.save(destination, format="webp")  # Convert image to webp
 
-    return destination
+    return destination #returning path to created image
 if result and url_ss:
     url = url_ss
-    driver.get(url)
+    driver.get(url) #accessing the given url
     title = driver.title
     
     sleep(1)
-    driver.get_screenshot_as_file("screenshots/screenshot.png")
+    driver.get_screenshot_as_file("screenshots/screenshot.png") #screenshot taken of the given url
     check = convert_to_webp(Path('screenshots/screenshot.png'), name_of_site)
     if check:
         image = Image.open(check)
