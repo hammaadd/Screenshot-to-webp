66a60c8e3ef4d256db2e168637e77d8ee8beb3df
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2023-06-02 13:23:16 +0500

    Disable Gpu Option Removed

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ def web_driver():
     options.add_argument("--verbose")
     options.add_argument('--no-sandbox')
     options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
     options.add_argument("--window-size=1920, 947")
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
